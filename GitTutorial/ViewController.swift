b296a05b68b15e4d7c82d1ceb8a68ded826f03ac
Beatriz Sanz <beatriz.sanz@upm.es>
2019-07-15 19:32:10 +0200

    First commit manually

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Beatriz Sanz Merino on 15/07/2019.
+//  Copyright © 2019 upm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let message = "Hello Git!"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(message)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
